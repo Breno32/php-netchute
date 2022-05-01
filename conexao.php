1c509a68f2d26ae8e0e3cbad9de3a0c78d327c09
Breno32 <77422180+Breno32@users.noreply.github.com>
2022-05-01 14:16:58 -0300

    subindo projeto

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b94509d45e68fc', '986c93f2', 'us-cdbr-east-05.cleardb.net') or die('Erro ao conectar ao banco de dados');
